ddc4b85820df53d8af9322ced87b3278a9de5678
awesomo4000 <alrhodes@gmail.com>
2025-10-05 05:49:12 -0400

    Remove generated files from git tracking
    The aarch64-macos-none/ directory contains generated code (21 files)
    that are created by the build system on each build via mkdir -p and
    code generation steps. These should not be tracked in git.
    
    - Removed aarch64-macos-none/ from git (22 files)
    - Added to .gitignore

--- aarch64-macos-none/erl_version.h
+++
@@ -1,5 +0,0 @@
-/* This file was created by 'make_version' -- don't modify. */
-#define ERLANG_OTP_RELEASE "OTP-28.1"
-#define ERLANG_OTP_VERSION "28.1"
-#define ERLANG_VERSION "16.1"
-#define ERLANG_ARCHITECTURE "aarch64-macos-none"
